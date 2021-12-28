3c5c00f14b4e7d56d7548a95815d23e15b1a6e58
palka <7paulina.sadowska@gmail.com>
2021-12-28 20:14:49 +0100

    basic layout

--- app/src/main/java/com/paulinasadowska/composeplayground/MainActivity.kt
+++ app/src/main/java/com/paulinasadowska/composeplayground/MainActivity.kt
@@ -3,11 +3,18 @@ package com.paulinasadowska.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.paulinasadowska.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +24,7 @@ class MainActivity : ComponentActivity() {
             ComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Paulina")
+                    MessageCard(Message(author = "Paulina", body = "Lorem ipsum..."))
                 }
             }
         }
@@ -25,14 +32,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(message: Message) {
+    Row(modifier = Modifier.padding(all = 4.dp)) {
+        Image(
+                painter = painterResource(id = R.drawable.profile_picture),
+                contentDescription = "profile image",
+                modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = message.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = message.body)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePlaygroundTheme {
-        Greeting("test name")
+        MessageCard(Message(author = "Test name", body = "Lorem ipsum..."))
     }
 }
+
+data class Message(val author: String, val body: String)
