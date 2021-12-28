e9448a69ebbc7d26d98967e5437d560e2dbf276c
palka <7paulina.sadowska@gmail.com>
2021-12-28 20:02:16 +0100

    empty project

---
+++ app/src/main/java/com/paulinasadowska/composeplayground/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.paulinasadowska.composeplayground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.paulinasadowska.composeplayground.ui.theme.ComposePlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePlaygroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Paulina")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposePlaygroundTheme {
+        Greeting("test name")
+    }
+}
