03adaea2e09ee58d38d6228abf885ec975b56b98
palka <7paulina.sadowska@gmail.com>
2021-12-29 21:40:46 +0100

    3: Material design

--- app/src/main/java/com/paulinasadowska/composeplayground/MainActivity.kt
+++ app/src/main/java/com/paulinasadowska/composeplayground/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.paulinasadowska.composeplayground
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -40,18 +42,38 @@ fun MessageCard(message: Message) {
                 modifier = Modifier
                         .size(40.dp)
                         .clip(CircleShape)
+                        .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = message.author)
+            Text(
+                    text = message.author,
+                    color = MaterialTheme.colors.secondary,
+                    style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = message.body)
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                        text = message.body,
+                        modifier = Modifier.padding(4.dp),
+                        style = MaterialTheme.typography.body2
+                )
+            }
+
         }
     }
 
 }
 
-@Preview(showBackground = true)
+@Preview(
+        showBackground = true,
+        name = "Light mode"
+)
+@Preview(
+        uiMode = UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark mode"
+)
 @Composable
 fun DefaultPreview() {
     ComposePlaygroundTheme {
