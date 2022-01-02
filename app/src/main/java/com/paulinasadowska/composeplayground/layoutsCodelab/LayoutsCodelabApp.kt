70e0a53699b2ef807a1a56a1f0923c8750397770
palka <7paulina.sadowska@gmail.com>
2022-01-02 21:10:00 +0100

    Change Card Layout

--- app/src/main/java/com/paulinasadowska/composeplayground/layoutsCodelab/LayoutsCodelabApp.kt
+++ app/src/main/java/com/paulinasadowska/composeplayground/layoutsCodelab/LayoutsCodelabApp.kt
@@ -1,6 +1,10 @@
 package com.paulinasadowska.composeplayground.layoutsCodelab
 
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,9 +17,18 @@ fun LayoutsCodelabApp() {
             topBar = { LayoutsCodelabAppBar() },
             bottomBar = { LayoutsCodelabBottomBar() },
     ) { innerPadding ->
-        PhotographerCard(Modifier.padding(innerPadding))
+        PhotographerCardList(Modifier.padding(innerPadding))
     }
+}
 
+@Composable
+fun PhotographerCardList(modifier: Modifier = Modifier){
+    val scrollState = rememberScrollState()
+    Column(modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            PhotographerCard(index = it)
+        }
+    }
 }
 
 @Composable
--- app/src/main/java/com/paulinasadowska/composeplayground/layoutsCodelab/PhotographerCard.kt
+++ app/src/main/java/com/paulinasadowska/composeplayground/layoutsCodelab/PhotographerCard.kt
@@ -1,48 +1,44 @@
 package com.paulinasadowska.composeplayground.layoutsCodelab
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.paulinasadowska.composeplayground.ui.theme.ComposePlaygroundTheme
 
 @Composable
-fun PhotographerCard(modifier: Modifier = Modifier) {
-    Row(modifier
-            .padding(8.dp)
-            .clip(RoundedCornerShape(4.dp))
-            .background(MaterialTheme.colors.primary)
-            .clickable { }
-            .padding(16.dp),
+fun PhotographerCard(modifier: Modifier = Modifier, index: Int = 0) {
+    Card(
+            backgroundColor = MaterialTheme.colors.primary,
+            modifier = modifier.padding(8.dp)
     ) {
-        Surface(
-                modifier = Modifier.size(50.dp),
-                shape = CircleShape,
-                color = MaterialTheme.colors.onPrimary.copy(alpha = 0.2f)
-        ) {
+        Row(Modifier.padding(16.dp)) {
+            Surface(
+                    modifier = Modifier.size(50.dp),
+                    shape = CircleShape,
+                    color = MaterialTheme.colors.onPrimary.copy(alpha = 0.2f)
+            ) {
 
-        }
-        Column(Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
-        ) {
-            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+            Column(Modifier
+                    .padding(start = 16.dp)
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text("Alfred Sisley $index", fontWeight = FontWeight.Bold)
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text("3 minutes ago", style = MaterialTheme.typography.body2)
+                }
             }
         }
     }
